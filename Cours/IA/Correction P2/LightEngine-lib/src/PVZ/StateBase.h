f968ea7d0691f21ff56ca64a9964ddff83a68f0f
Iker COT VAQUERO <icotvaquero@gaming.tech>
2026-01-30 15:29:33 +0100

    update

--- Cours/IA/Correction P2/LightEngine-lib/src/PVZ/StateBase.h
+++ Cours/IA/Correction P2/LightEngine-lib/src/PVZ/StateBase.h
@@ -7,5 +7,5 @@ public:
 	virtual void Start(T* plant) = 0;
 	virtual void Update(T* plant, float dt) = 0;
 	virtual void End(T* plant) = 0;
-	virtual bool CanTransitionFrom(T* plant, int currentState) = 0;
+	virtual bool CanTransitionFrom(T* plant, int currentState) { return true; };
 };
